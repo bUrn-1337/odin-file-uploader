d46e778a8d76a70d22dfc6c082b34daace0b4e4b
Nameless <satyamsharma2106@gmail.com>
2025-07-02 19:54:56 +0530

    add special function to get root folder for a user

--- src/controllers/folderController.js
+++ src/controllers/folderController.js
@@ -1,8 +1,7 @@
-const { insertFolder } = require("../services/folderService");
-exports.createFolder = async (req, res) => {
+const { insertFolder, getFolder, updateFolder, deleteFolder } = require("../services/folderService");
+const createFolder = async (req, res) => {
     const { parentPath } = req.params;
     const { name } = req.body;
-
     let parent;
     if (parentPath.split("/").length > 0) {
         parent = parentPath.split("/")[-1];
@@ -10,4 +9,34 @@ exports.createFolder = async (req, res) => {
         parent = "home";
     }
     await insertFolder(name, parent);
+}
+
+const getFolderHandler = async (req, res) => {
+    let { path } = req.params;
+    if (!path) {
+        return res.render()
+    }
+    const userId = req.user.id; 
+    path = path.split("/");
+    const name = path.splice(-1, 1);
+    const folder = await getFolder(name, path, userId);
+    res.render("index", {
+        folder,
+    });
+}
+
+const updateFolderHandler = async (req, res) => {
+
+}
+
+const deleteFolderHandler = async (req, res) => {
+
+}
+
+module.exports = {
+    createFolder,
+    getFolderHandler,
+    updateFolderHandler,
+    deleteFolderHandler,
+
 }
--- src/routes/folder.js
+++ src/routes/folder.js
@@ -3,6 +3,9 @@ const router = Router();
 const folderController = require("../controllers/folderController");
 const { ensureAuthenticated } = require("../middlewares/auth");
 
-router.post("/:parentPath", ensureAuthenticated, folderController.createFolder);
+router.get("/:path", ensureAuthenticated, folderController.getFolderHandler);
+router.post("/:path", ensureAuthenticated, folderController.createFolderHandler);
+router.put("/:path", ensureAuthenticated, folderController.updateFolderHandler);
+router.delete("/:path", ensureAuthenticated, folderController.deleteFolderHandler);
 
 module.exports = router;
